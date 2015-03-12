a99021c1658827e76aa1d50cc1814b3a8bfcde04
Rehno Lindeque <errantkid@gmail.com>
2015-03-12 22:12:57 +0200

    Added coffee style existential operator

--- src/Maybe/Extra.elm
+++ src/Maybe/Extra.elm
@@ -1,17 +1,25 @@
 module Maybe.Extra where
 {-| Convenience functions for Maybe.
 
-@docs join
+@docs (?), join
 -}
 
 import Maybe (..)
+
+{-| Flipped, infix version of `withDefault`.
+
+    head [] ? 0 == 0
+-}
+(?) : Maybe a -> a -> a
+mx ? x = withDefault x mx
+
+
 
 {-| Flattens nested Maybes
 
     join (Just (Just 1)) == Just 1
     join (Just Nothing)  == Nothing
     join Nothing         == Nothing
-
 -}
 join : Maybe (Maybe a) -> Maybe a
 join mx =
