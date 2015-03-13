b3d861fa19c9c71bd4708f471c22e4cae8d24ddd
jvoigtlaender <jvoigtlaender@users.noreply.github.com>
2015-03-13 11:21:56 +0100

    Introduced isNothing
    ... with the documentation it once had in the standard lib.

--- src/Maybe/Extra.elm
+++ src/Maybe/Extra.elm
@@ -1,7 +1,7 @@
-module Maybe.Extra where
+module Maybe.Extra ((?), join, isNothing) where
 {-| Convenience functions for Maybe.
 
-@docs (?), join
+@docs (?), join, isNothing
 -}
 
 import Maybe (..)
@@ -26,3 +26,17 @@ join mx =
   case mx of
     Just x -> x
     Nothing -> Nothing
+
+
+{-| Conveniently check if constructed with `Nothing`.
+
+      isNothing (Just 42) == False
+      isNothing (Just []) == False
+      isNothing Nothing   == True
+-}
+isNothing : Maybe a -> Bool
+isNothing m =
+    case m of
+      Nothing -> True
+      Just _  -> False
+
